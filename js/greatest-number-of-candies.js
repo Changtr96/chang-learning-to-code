d25c1f8c9da04af08e67574f1d6cf0d64cfbb573
chang <changgdesigner@gmail.com>
2024-04-23 09:51:15 +0700

    greatestNumberOfCandies improvement

--- js/greatest-number-of-candies.js
+++ js/greatest-number-of-candies.js
@@ -1,12 +1,7 @@
 function greatestNumberOfCandies(candies, extraCandies) {
   const result = [];
-  let max = candies[0];
-  for (let i = 1; i < candies.length; i++) {
-    if (candies[i] > max) {
-      max = candies[i];
-    }
-  }
   for (let i = 0; i < candies.length; i++) {
+    const max = Math.max(...candies);
     if (candies[i] + extraCandies >= max) {
       result.push(true);
     } else {
