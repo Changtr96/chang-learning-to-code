c330b3c0bbdab92337bccdb96ecc4ab0bddb5a34
Khang Nguyễn <thinhkhang97@gmail.com>
2024-05-12 20:32:53 +0700

    fix fetch data not work

--- pokemon/index.js
+++ pokemon/index.js
@@ -3,28 +3,27 @@ const pokemonNameInput = document.querySelector("#pokemon-name-input");
 const searchBtn = document.querySelector("#search-btn");
 const pokemonInfo = document.querySelector("#pokemon-info");
 fetchPokemon.then((response) => response.json()).then(showPokemon);
+
+/**
+ * show pokemons
+ * @param {{count: number; results: {name: string; url: string}[]}} data
+ */
 function showPokemon(data) {
-  const searchValue = pokemonNameInput.value;
-  for (i = 0; i < data.length; i++)
-    if (searchValue === data[i].name) {
-      const urlPokemon = data[i].url;
-      const getPokemonInfo = fetch(urlPokemon);
-      const imgPokemon = document.createElement("img");
-      const namePokemon = document.createElement("p");
-
-      imgPokemon.src =
-        getPokemonInfo.sprites.other["official-artwork"].front_default;
-      namePokemon.innerText = data[i].name;
-      pokemonInfo.appendChild(imgPokemon);
-      pokemonInfo.appendChild(namePokemon);
-      console.log(getPokemonInfo);
-    } else {
-      const empty = document.createElement("p");
-      empty.innerText = "not found";
-    }
-
-  console.log(searchValue);
-  console.log(data);
+  const pokemonList = data.results;
+  for (i = 0; i < pokemonList.length; i++) {
+    const urlPokemon = pokemonList[i].url;
+    const getPokemonInfo = fetch(urlPokemon);
+    getPokemonInfo
+      .then((response) => response.json())
+      .then((info) => {
+        const imgPokemon = document.createElement("img");
+        const namePokemon = document.createElement("p");
+        imgPokemon.src = info.sprites.other["official-artwork"].front_default;
+        namePokemon.innerText = info.name;
+        pokemonInfo.appendChild(imgPokemon);
+        pokemonInfo.appendChild(namePokemon);
+      });
+  }
 }
 
 searchBtn.addEventListener("click", showPokemon);
